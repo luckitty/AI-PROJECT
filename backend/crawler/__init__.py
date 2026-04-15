64c3dc194f6692321d581dbf711c41dd117db2ee
luckitty <15510253635@163.com>
2026-04-15 16:56:39 +0800

    chore: stop tracking crawler and note_images directories

--- backend/crawler/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Xiaohongshu crawler package."""
-
-from .config import CrawlerConfig
-from .runner import run_crawler, run_travel_route_agent
-
-__all__ = ["CrawlerConfig", "run_crawler", "run_travel_route_agent"]
